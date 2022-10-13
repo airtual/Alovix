ad22ebcd98cb78650156a8fe0d652272d70e4155
airtual <77363680+airtual@users.noreply.github.com>
2022-10-13 11:36:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const stylesheet = document.documentElement.style;
 
 //among us
+if (theme=dark) {
+ "document.getElementById('body').style.color = '#232527'" 
+  "document.getElementById('navbar').style.color = '#191d1b'"
+}
